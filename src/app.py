f6cf6c21581ac29e4e081cbfa1ed7d1d4e6dd149
Marcos Augusto Silva <136196505+marcosAugustoo@users.noreply.github.com>
2025-07-28 15:20:07 -0300

    criação de modelos de dados

--- src/app.py
+++ src/app.py
@@ -2,7 +2,10 @@ import os
 import click
 
 from flask import Flask, current_app
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
+import sqlalchemy as sa
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
@@ -14,12 +17,23 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 
 
-class User:
-    pass
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r})"
 
 
-class Post:
-    pass
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    body: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey('user.id'))
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
 
 
 @click.command('init-db')
