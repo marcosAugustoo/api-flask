768e234fdf7062d0aaac171dc3c76052ab01fb52
Marcos Augusto Silva <136196505+marcosAugustoo@users.noreply.github.com>
2025-07-28 20:08:45 -0300

    criacao da funcao _list_users
    criação da função _list_users() servindo como Read do CRUD

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -12,10 +12,16 @@ def _create_user():
     db.session.commit()
 
 
+def _list_users():
+    query = db.select(User)
+    users = db.session.execute(query).scalars()
+    return [{'id': user.id, 'username': user.username} for user in users]
+
+
 @app.route('/', methods=['GET', 'POST'])
 def handle_user():
     if request.method == 'POST':
         _create_user()
         return {'message': 'User created.'}, HTTPStatus.CREATED
     else:
-        return {'users': []}
+        return {'users': _list_users()}
