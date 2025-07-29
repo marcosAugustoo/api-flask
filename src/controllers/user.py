dc9aef4d5ebc3e5a2f451cda3ab040e717fc6893
Marcos Augusto Silva <136196505+marcosAugustoo@users.noreply.github.com>
2025-07-29 09:16:45 -0300

    criacao do metodo update com sqlalchemy inspector()

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from src.app import User, db
 from http import HTTPStatus
+from sqlalchemy import inspect
 
 app = Blueprint('user', __name__, url_prefix='/users')
 
@@ -30,6 +31,23 @@ def handle_user():
 @app.route('/<int:user_id>')
 def get_user(user_id):
     user = db.get_or_404(User, user_id)
+    return {
+        "id": user.id,
+        "username": user.username,
+    }
+
+
+@app.route('/<int:user_id>', methods=['PATCH'])
+def update_user(user_id):
+    user = db.get_or_404(User, user_id)
+    data = request.json
+
+    mapper = inspect(User)
+    for column in mapper.attrs:
+        if column.key in data:
+            setattr(user, column.key, data[column.key])
+    db.session.commit()
+
     return {
         "id": user.id,
         "username": user.username,
